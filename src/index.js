2912ae73f56c0b9b4ae36f0ed7f8ba752ff9403e
Maria <bondarenkam@gmail.com>
2017-09-18 00:22:54 +0300

    Brackets:implement check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let st = []; // массив открытых скобок
+
+  for (let i = 0; i < str.length; i++) {
+    let simbol = str[i];
+    let index;
+    for (let j = 0; j < bracketsConfig.length; j++) {
+      let a = bracketsConfig[j][0];
+      let b = bracketsConfig[j][1];
+      if (simbol == b) {
+        if (st.length || a == b) {
+          if (st[st.length - 1] == a) {
+            let pop = st.pop();
+            break;
+          }
+        } else {
+          return false;
+        }
+      }
+      if (simbol == a) {
+        st.push(simbol);
+        break;
+      }
+    }
+  }
+  if (!st.length) {
+    return true;
+  }
+  return false;
 }
